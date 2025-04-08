77f4055dc7f698554e6b105d197b881f94430739
Teh-Mouse <125417205+Teh-Mouse@users.noreply.github.com>
2025-04-08 00:48:52 -0400

    Update main.py
    Changed the calendar display from a singular button to a dropdown. Allows for future display in week and day? formats

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.popup import Popup
 from kivy.uix.button import Button
+from kivy.uix.spinner import Spinner
+from kivy.uix.dropdown import DropDown
 
 date = datetime.datetime.now()
 
@@ -28,13 +30,22 @@ def fetchDay():
     """
     Function to find the corresponding school day to the current date.
     """
+    global df
+    global monthslice
+    #global rowseries
+    global slice2
     df = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vSEmrxJzhnV_wvnd2GkiyuVoBviY8kZOhGhBZd7EsraGpzn-9wmCycgWZXAr8tYXSJiBM2GQ-jeLvIt/pub?gid=0&single=true&output=csv',dtype={"MONTH" : np.str_})
     print(df)
     rowseries=np.where(df["MONTH"]==str(date.month))
     print(rowseries)
-    rowslice=df[df["MONTH"]==str(date.month)]
-    print(rowslice)
-    columnseries=np.where(rowslice.astype(np.str_)==str(date.day)) # must be compared as type str due to NaN values
+    monthslice=df[df["MONTH"]==str(date.month)]
+    slice2=df.iloc[rowseries[0]+1] #next slice row
+    print(monthslice) #loc or iloc are not viable as the indexing is via the month column not the dataframe itself's row indexing
+    columnseries=np.where(monthslice.astype(np.str_)==str(date.day)) # must be compared as type str due to NaN values
+    #print(date.day)
+    #df[]
+    #print(df.loc[[str(date.day)]])
+    #columnseries=np.where(df.loc[[date.day]]==str(date.day))
     print(columnseries)
     abcdef=rowseries[0][0],columnseries[1][0]
     print(abcdef)
@@ -48,6 +59,7 @@ class testGrid(GridLayout):
     """
     Main class to create the user interface.
     """
+    #global monthslice
     def __init__(self,**kwargs):
         formatted_date = str((str(date.year) + "/" + str(date.month) + "/" + str(date.day)))
        # fetchDay(datetime.date.month,datetime.date.day)
@@ -67,17 +79,38 @@ class testGrid(GridLayout):
         templist="MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY"
         for x in templist:
             self.calendarholder.add_widget(Label(text=x))
-        self.calendarholder.add_widget(Label(text='Hello world'))
-        for i in range(30-1+1):
-            self.calendarholder.add_widget(Label(text=f"Date {i+1}"))
-        self.hello=Popup(title='Test calendar popup (month)', content=self.calendarholder, size_hint=(None, None), size=(1000, 500))
-        self.calbutton=Button(text="click here")
-        self.calbutton.bind(on_press=self.hello.open)
+        #self.calendarholder.add_widget(Label(text='Hello world'))
+        #for i in range(30-1+1):
+        #    self.calendarholder.add_widget(Label(text=f"Date {i+1}"))
+        for i in range(len(monthslice.iloc[0])-1): #1st row is the month
+            self.calendarholder.add_widget(Label(text=f"{monthslice.iloc[0][i+1]}\nDay {slice2.iloc[0][i+1]}"))
+        self.monthdisplay=Popup(title=f'Test calendar popup for the month of {monthslice.iloc[0][0]}', content=self.calendarholder, size_hint=(None, None), size=(1000, 500))
+        self.calbutton=Button(text="click here",size_hint_y=0.25)
+        #self.calbutton.bind(on_press=self.monthdisplay.open)
+        self.displaydropdown=DropDown()
+        #self.displaydropdown.bind(on_release=self.monthdisplay.open)
+        for i in range(3):
+            btn=Button(text=f"Button {i}",size_hint=(None,None),height=50)
+            btn.bind(on_release=lambda btn:self.displaydropdown.select(btn.text))
+            btn.bind(on_release=self.monthdisplay.open)
+            self.displaydropdown.add_widget(btn)
+        #self.anotherbutton=Button(text="auugh")
+        #self.anotherbutton.bind(on_release=self.displaydropdown.open)
+        self.displaydropdown.bind(on_select=lambda instance,x:setattr(self.calbutton,"text",f"{x} selected")) #changes display of calbutton
+        #self.add_widget(self.anotherbutton)
+        self.calbutton.bind(on_press=self.displaydropdown.open)
         self.add_widget(self.calbutton)
 
         self.add_widget(TextInput(text="Text Box:"))
         for i in range(3):
             self.add_widget(Label(text="Placeholder"))
+        #self.displaydropdown=Spinner(text="clikc me!",values=("Display by Day",'Display by Week',"Display by Month"),size_hint=(None,None))
+        #self.displaydropdown.bind(text=show_selected_value)
+
+        #self.displaydropdown.bind(on_press=stupid(self.displaydropdown,self.displaydropdown.text))
+        #stupid()
+
+ 
 # App Runtime #
 
 class MyApp(App):
